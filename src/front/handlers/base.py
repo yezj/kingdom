1014f034cc919763d25bc55496048537830a8bc4
2851827398@qq.com <2851827398@qq.com>
2017-09-20 15:41:42 +0800

    modify kingdom

--- src/front/handlers/base.py
+++ src/front/handlers/base.py
@@ -163,7 +163,7 @@ class BaseHandler(web.RequestHandler, storage.DatabaseMixin):
                           buddyList, playerStatusInfo, annalNormal, annelCurrentGateNormal, annalHero,
                           annelCurrentGateHero, annalEpic, dungeonAnnelHero, dungeonAnnelEpic, dungeonAnnelGatesNormal,
                           dungeonAnnelGatesHero, dungeonAnnelGatesEpic, jmails, int(time.time()), int(time.time()))
-                print query % params
+                #print query % params
                 for i in range(5):
 
                     try:
