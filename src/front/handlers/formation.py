e3fa06911360acf3a1910fecb4e7d9f6417e0d7f
2851827398@qq.com <2851827398@qq.com>
2017-10-17 09:14:53 +0800

    modify kingdom

--- src/front/handlers/formation.py
+++ src/front/handlers/formation.py
@@ -99,17 +99,17 @@ class SetHandler(ApiHandler):
             print 'formations', formations
             #print escape.json_encode(dict(slotId=slotId, formation=formation))
 
-            # query = "UPDATE core_user SET formations=%s WHERE hex=%s and id=%s"
-            # params = (escape.json_encode(formations), ahex, aid)
-            # for i in range(5):
-            #     try:
-            #         yield self.sql.runOperation(query, params)
-            #         break
-            #     except storage.IntegrityError:
-            #         log.msg("SQL integrity error, retry(%i): %s" % (i, (query % params)))
-            #         continue
-            # ret = dict(timestamp=int(time.time()))
-            # reb = zlib.compress(escape.json_encode(ret))
+            query = "UPDATE core_user SET formations=%s WHERE hex=%s and id=%s"
+            params = (escape.json_encode(formations), ahex, aid)
+            for i in range(5):
+                try:
+                    yield self.sql.runOperation(query, params)
+                    break
+                except storage.IntegrityError:
+                    log.msg("SQL integrity error, retry(%i): %s" % (i, (query % params)))
+                    continue
+            #ret = dict(timestamp=int(time.time()))
+            #reb = zlib.compress(escape.json_encode(ret))
             self.write(formations)
         else:
             self.write(dict(err=E.ERR_ARGUMENT, msg=E.errmsg(E.ERR_ARGUMENT)))
