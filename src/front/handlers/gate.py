170d857e1c1fa6505aaf1638db5591c665922abf
yezijian <2851827398@qq.com>
2018-04-19 15:29:33 +0800

    modify name_2P

--- src/front/handlers/gate.py
+++ src/front/handlers/gate.py
@@ -81,7 +81,7 @@ class GetHandler(ApiHandler):
                           barrie=escape.json_decode(barrie),
                           wave1P=escape.json_decode(wave1P),
                           wave2P=escape.json_decode(wave2P),
-                          name_2P=name_2P,
+                          name_2P=escape.json_decode(name_2P),
                           level_2P=level_2P,
                           icon_2P=icon_2P,
                           herosNum1P=herosNum1P,
